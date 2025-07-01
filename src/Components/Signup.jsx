dd3670b3676cd920fe823f442740ad0f6f94f86e
B Bhanuprasad <bhanuprasadbingi1999@gmail.com>
2025-07-01 11:54:02 +0530

    api changed

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/signup', {
+      const res = await axios.post('https://mybackend5-7641.onrender.com/api/auth/signup', {
         fullname,
         email,
         password,
